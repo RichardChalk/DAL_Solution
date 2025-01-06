582371b871c8da6150cc09f9275403cd178de648
Richard Chalk <barmybrit@gmail.com>
2025-01-06 20:13:55 +0100

    Cleaned upp som classes

--- Project 2/Program.cs
+++ Project 2/Program.cs
@@ -1,22 +1,21 @@
 ﻿using DataAccessLayer;
 using DataAccessLayer.Models;
 
-namespace Project_2
+namespace Project_2;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Jag kan också få tillgång till Northwind genom min DataAccessLayer!
+        AccessDatabas accessDatabas = new AccessDatabas();
+        using (NorthwindContext context = accessDatabas.GetDbContext())
         {
-            // Jag kan också få tillgång till Northwind genom min DataAccessLayer!
-            AccessDatabas accessDatabas = new AccessDatabas();
-            using (NorthwindContext context = accessDatabas.GetDbContext())
+            // Produkter!
+            var products = context.Products.ToList();
+            foreach (var p in products)
             {
-                // Produkter!
-                var products = context.Products.ToList();
-                foreach (var p in products)
-                {
-                    Console.WriteLine($"{p.ProductId}, {p.ProductName}");
-                }
+                Console.WriteLine($"{p.ProductId}, {p.ProductName}");
             }
         }
     }
