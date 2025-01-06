10721809563c3717a9ae27eb1d3ba096fe3211d6
Richard Chalk <barmybrit@gmail.com>
2025-01-06 18:25:03 +0100

    Add project files.

---
+++ DataAccessLayer/AccessDatabas.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccessLayer;
+
+public class AccessDatabas
+{
+    public NorthwindContext GetDbContext()
+    {
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json", true, true);
+        var config = builder.Build();
+
+        var connectionString = config.GetConnectionString("DefaultConnection");
+
+        var options = new DbContextOptionsBuilder<NorthwindContext>();
+        options.UseSqlServer(connectionString);
+
+        return new NorthwindContext(options.Options);
+    }
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Models;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Jag kan få tillgång till Northwind genom min DataAccessLayer!
+            AccessDatabas accessDatabas = new AccessDatabas();
+            using (NorthwindContext context = accessDatabas.GetDbContext())
+            {
+                // Customers
+                var customers = context.Customers.ToList();
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine($"{customer.CustomerId}, {customer.CompanyName}");
+                }
+            }
+        }
+    }
+}
